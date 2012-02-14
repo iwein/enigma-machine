af681b45d8504ddb084454102b34873e55313d6f
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2012-02-14 17:07:23 +0100

    Added alphabets

---
+++ project/src/main/java/nl/fnord/xke/enigma/Alphabets.java
@@ -0,0 +1,8 @@
+package nl.fnord.xke.enigma;
+
+public class Alphabets {
+  public final String realAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  public final String alphabetI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+  public final String alphabetII = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+  public final String alphabetIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+}
