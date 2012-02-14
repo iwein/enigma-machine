a992de420a3bec7b67cf1a2997d04a7c59996b0e
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2012-02-14 17:32:37 +0100

    Static strings.

--- project/src/main/java/nl/fnord/xke/enigma/Alphabets.java
+++ project/src/main/java/nl/fnord/xke/enigma/Alphabets.java
@@ -1,10 +1,10 @@
 package nl.fnord.xke.enigma;
 
 public class Alphabets {
-  public final String realAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  public final String alphabetI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-  public final String alphabetII = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
-  public final String alphabetIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+  public final static String realAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  public final static String alphabetI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+  public final static String alphabetII = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+  public final static String alphabetIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
   
-  public final String reflector = "ABCDEFGDIJKGMKMIEBFTCVVJAT";
+  public final static String reflector = "ABCDEFGDIJKGMKMIEBFTCVVJAT";
 }
