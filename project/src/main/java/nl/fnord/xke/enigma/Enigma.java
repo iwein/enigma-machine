4111fd54e7ccdbf7836b2acf3acb24373f84b5df
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2012-02-14 17:53:13 +0100

    Implemented input translation.

--- project/src/main/java/nl/fnord/xke/enigma/Enigma.java
+++ project/src/main/java/nl/fnord/xke/enigma/Enigma.java
@@ -1,13 +1,22 @@
 package nl.fnord.xke.enigma;
 
+import static nl.fnord.xke.enigma.Alphabets.realAlphabet;
 public class Enigma {
 
     public String transform(String clearText) {
         throw new UnsupportedOperationException("Please, implement this method.");
     }
 
+    /**
+	 * Translates an input character to an input index for input into the
+	 * rotors.
+	 * 
+	 * @param input
+	 *            the input character.
+	 * @return index for the input character
+	 */
     public int type(char input) {
-        return 4;
+    	return realAlphabet.indexOf(input);
     }
 
 }
