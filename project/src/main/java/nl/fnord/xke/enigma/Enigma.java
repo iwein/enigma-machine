4339b3327bfe62c42d5768f90695db19cf14393d
Barend Garvelink <bgarvelink@xebia.com>
2012-02-14 19:01:15 +0100

    Added a failing test for the rotor 3 translation.

--- project/src/main/java/nl/fnord/xke/enigma/Enigma.java
+++ project/src/main/java/nl/fnord/xke/enigma/Enigma.java
@@ -8,15 +8,22 @@ public class Enigma {
     }
 
     /**
-	 * Translates an input character to an input index for input into the
-	 * rotors.
-	 * 
-	 * @param input
-	 *            the input character.
-	 * @return index for the input character
-	 */
+     * Translates an input character to an input index for input into the
+     * rotors.
+     *
+     * @param input
+     *            the input character.
+     * @return index for the input character
+     */
     public int type(char input) {
     	return realAlphabet.indexOf(input);
+    }
+
+    /**
+     * Translates the character index of rotor III.
+     */
+    public int rotor3(int input) {
+        throw new UnsupportedOperationException("Please, implement this method.");
     }
 
 }
