9ef826f73101ce05207bb4f2ff4c4bc770d49c86
Barend Garvelink <bgarvelink@xebia.com>
2012-02-14 17:11:36 +0100

    Added regretfully absent JUnit annotation

--- project/src/test/java/nl/fnord/xke/enigma/AcceptanceTest.java
+++ project/src/test/java/nl/fnord/xke/enigma/AcceptanceTest.java
@@ -3,10 +3,13 @@ package nl.fnord.xke.enigma;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
 public class AcceptanceTest {
     private String clearText = "ENIGMA REVEALED";
     private String cipherText = "QMJIDO MZWZJDMG";
 
+    @Test
     public void accept() {
         Enigma enigma = new Enigma();
         assertThat(enigma.transform(clearText), is(cipherText));
