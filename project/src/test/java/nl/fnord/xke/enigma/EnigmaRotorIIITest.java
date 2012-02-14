f1e2ce38c593d1ecd3b664a54d445d8a6128dbf5
Barend Garvelink <bgarvelink@xebia.com>
2012-02-14 19:11:37 +0100

    Made room for a Rotor1 and Rotor2 test alongside the existing Rotor3 test.

--- project/src/test/java/nl/fnord/xke/enigma/EnigmaRotorIIITest.java
+++ project/src/test/java/nl/fnord/xke/enigma/EnigmaRotorIIITest.java
@@ -1,50 +1,30 @@
 package nl.fnord.xke.enigma;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static nl.fnord.xke.enigma.Alphabets.alphabetIII;
 import static nl.fnord.xke.enigma.Alphabets.realAlphabet;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import java.util.List;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import quicktime.qd3d.transform.RotateTransformData;
-
-import com.google.common.collect.Lists;
 
 /**
- * A full test of the transformation from the keyboard input to the position on the first rotor.
- * <p/>
- * I've never used JUnit's {@link Parameterized} runner before, let's check it out.
+ * A full test of the transformation from the keyboard input to the position on the third rotor.
  */
 @RunWith(Parameterized.class)
-public class EnigmaRotorIIITest {
-
-    private Enigma enigma = new Enigma();
-    private int input;
-    private int expectedIndex;
+public class EnigmaRotorIIITest extends EnigmaRotorTest {
 
     public EnigmaRotorIIITest(int input, int expectedIndex) {
-        super();
-        this.input = input;
-        this.expectedIndex = expectedIndex;
+        super(input, expectedIndex);
     }
 
-    @Test
-    public void test() {
-        assertThat(enigma.rotor3(input), is(expectedIndex));
+    @Override
+    int transform(int param, Enigma enigma) {
+        return enigma.rotor3(param);
     }
 
     @Parameters
     public static List<Object[]> data() {
-        List<Object[]> result = newArrayList();
-        for (int i = 0, max = realAlphabet.length(); i < max; i++) {
-            result.add(new Object[] { realAlphabet.charAt(i), alphabetIII.charAt(i) });
-        }
-        return result;
+        return data(realAlphabet, alphabetIII);
     }
 }
---
+++ project/src/test/java/nl/fnord/xke/enigma/EnigmaRotorTest.java
@@ -0,0 +1,43 @@
+package nl.fnord.xke.enigma;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static nl.fnord.xke.enigma.Alphabets.alphabetIII;
+import static nl.fnord.xke.enigma.Alphabets.realAlphabet;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Common code for enigma rotor tests.
+ */
+public abstract class EnigmaRotorTest {
+
+    private Enigma enigma = new Enigma();
+    private int input;
+    private int expectedIndex;
+
+    public EnigmaRotorTest(int input, int expectedIndex) {
+        super();
+        this.input = input;
+        this.expectedIndex = expectedIndex;
+    }
+
+    abstract int transform(int param, Enigma enigma);
+
+    @Test
+    public void test() {
+        assertThat(transform(input, enigma), is(expectedIndex));
+    }
+
+    static List<Object[]> data(String leftColumn, String rightColumn) {
+        assert leftColumn.length() == rightColumn.length();
+        List<Object[]> result = newArrayList();
+        for (int i = 0, max = realAlphabet.length(); i < max; i++) {
+            result.add(new Object[] { i, alphabetIII.indexOf(realAlphabet.charAt(i)) });
+        }
+        return result;
+    }
+}
