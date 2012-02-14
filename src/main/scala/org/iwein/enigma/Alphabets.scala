060f5efc7e62d5b1698aaa2351bea35114a5a660
Iwein Fuld <iwein@fuld.net>
2012-02-14 16:38:04 +0100

    Add alphabets and failing acceptance test

---
+++ src/main/scala/org/iwein/enigma/Alphabets.scala
@@ -0,0 +1,14 @@
+package org.iwein.enigma
+
+/**
+ * @author iwein
+ */
+
+trait Alphabets {
+
+  val realAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  val alphabetI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+  val alphabetII = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+  val alphabetIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+}
