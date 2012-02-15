082a5f6aa063f0cc1d2dc93479c00a831ff428b7
Arjan Molenaar <gaphor@gmail.com>
2012-02-15 18:00:11 +0100

    Working implementation, no notching.

--- enigma.py
+++ enigma.py
@@ -3,6 +3,7 @@
 # Design constaint: ignored notches.
 
 #ROTOT_BASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 ROTORS = [
   "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
@@ -12,8 +13,10 @@ ROTORS = [
 
 REFLECTOR = "ABCDEFGDIJKGMKMIEBFTCVVJAT"
 
-INITIAL_OFFSETS = ( 10, -8, 10 )
-#                   12,  2, 10
+INITIAL_OFFSETS = ( 'M', 'C', 'K' )
+
+def to_char(c):
+    return chr(ord('A') + c)
 
 def reflect(offset):
     """
@@ -22,35 +25,58 @@ def reflect(offset):
     >>> reflect(23)
     9
     """
-    return (REFLECTOR * 2).index(REFLECTOR[offset], offset + 1) % len(REFLECTOR)
+    return (REFLECTOR * 2).index(REFLECTOR[offset % 26], (offset % 26) + 1) % len(REFLECTOR)
 
-def transform_char(char, rotors, offsets):
+def to_index(rotor):
     """
-    >>> transform_char("E", ["BDFHJLCPRTXVZNYEIWGAKMUSQO"], (-10, 10))
-    'M'
-    >>> transform_char("V", ["BDFHJLCPRTXVZNYEIWGAKMUSQO"], (-10, 10))
-    'R'
+    >>> to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO")
+    [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
     """
-    index = ord(char) - ord('A')
+    return map(lambda c: ord(c) - ord('A'), rotor)
+
+def encode_left(offset, rotor):
+    """
+    >>> encode_left(14, to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO"))
+    24
+    >>> encode_left(32, to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO"))
+    2
+    """
+    return rotor[offset % 26]
+
+def encode_right(offset, rotor):
+    """
+    >>> encode_right(4, to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO")) # 4 == 'E'
+    15
+    >>> encode_right(16, to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO")) # 16 == 'Q'
+    24
+    >>> encode_right(25, to_index("BDFHJLCPRTXVZNYEIWGAKMUSQO")) # 16 == 'Q'
+    12
+    """
+    #print 'env right', offset
+    return rotor.index(offset % 26)
+
+
+def transform_char(index, rotors, offsets):
+    """
+    >>> transform_char(4, ["BDFHJLCPRTXVZNYEIWGAKMUSQO"], (-10, 10))
+    7
+    >>> transform_char(22, ["BDFHJLCPRTXVZNYEIWGAKMUSQO"], (-10, 10))
+    23
+    """
     offset = offsets[-1]
 
+    index = index + offset
+
     if not rotors:
-        return chr(ord('A') + reflect(index + offset))
-
-    rotor = rotors[-1]
+        index = reflect(index)
+    else:
+        rotor = to_index(rotors[-1])
     
-    enchar = rotor[(index + offset) % 26]
-    print 'rotor 1:', enchar
-    enchar = transform_char(enchar, rotors[:-1], offsets[:-1])
-    print ' aft tfm:', enchar
+        index = encode_left(index, rotor)
+        index = transform_char(index, rotors[:-1], offsets[:-1])
+        index = encode_right(index, rotor)
 
-    # And now, back...
-    index = ord(enchar) - ord('A')
-    enchar = rotor[(index + offset) % 26]
-    enchar = chr(ord('A') + index)
-    print 'rotor 2:', index, offset, enchar
-
-    return enchar
+    return (index - offset) % 26
 
 def increment_offset(ofs):
     """
@@ -63,8 +89,30 @@ def increment_offset(ofs):
     """
     return ofs[:-2] + ((ofs[-2]-1) % 26, (ofs[-1]+1) % 26)
 
+def relative_offsets(offsets):
+    """
+    >>> relative_offsets(('M', 'C', 'K'))
+    (-12, 10, -8, 10)
+    """
+    offsets = to_index(offsets)
+    offsets.reverse()
+    o = offsets[0:1] + [b-a for a, b in zip(offsets, offsets[1:])]
+    o.reverse()
+    return tuple([-sum(o)] + o)
+
 def transform(message, rotors, offsets):
-    ofs = offsets
+    """
+    >>> transform("ENIGMA REVEALED", ROTORS, INITIAL_OFFSETS)
+    QMJIDO MZWZJDMG
+    """
+    #import pdb
+    ofs = relative_offsets(offsets)
+    output = []
+    #pdb.set_trace()
     for c in message:
-        ofs = increment_offset(ofs)
-        transform_char(c.upper(), rotors, (-sum(ofs),) + ofs)
+        if ord('A') <= ord(c.upper()) <= ord('Z'):
+            ofs = increment_offset(ofs)
+            output.append(chr(transform_char(ord(c.upper()) - ord('A'), rotors, ofs) + ord('A')))
+        else:
+            output.append(c)
+    print "".join(output)
