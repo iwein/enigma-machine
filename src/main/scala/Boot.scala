836cc7dfac43607dbb0157f4399224892eea6248
Iwein Fuld <iwein@fuld.net>
2013-09-03 13:17:43 +0200

    Adds PORT detection to Boot

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,3 +1,4 @@
+import scala.util.Properties
 import spray.can.server.SprayCanHttpServerApp
 import com.weiglewilczek.slf4s.Logging
 import akka.actor.Props
@@ -5,7 +6,7 @@ import akka.actor.Props
 object Boot extends App with SprayCanHttpServerApp with Logging {
   try {
     val service = system.actorOf(Props(new RestActor()), "enigma-service")
-    newHttpServer(service) ! Bind("0.0.0.0", 8080)
+    newHttpServer(service) ! Bind("0.0.0.0", Properties.envOrElse("PORT", "9080").toInt)
   } catch {
     case e: Throwable => logger.error("Error while booting", e)
   }
