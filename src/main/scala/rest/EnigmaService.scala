d8ebdaaf25a61853c519bbd45ba1dc2d2ab63bc0
Iwein Fuld <iwein@fuld.net>
2013-09-03 10:19:14 +0200

    Integrates enigma into service

--- src/main/scala/rest/EnigmaService.scala
+++ src/main/scala/rest/EnigmaService.scala
@@ -3,8 +3,20 @@ package rest
 import spray.routing.HttpService
 import spray.httpx.SprayJsonSupport
 import spray.httpx.encoding.{Gzip, NoEncoding}
+import org.iwein.enigma.{Alphabets, Reflector, Rotor, Enigma}
 
-trait EnigmaService extends HttpService with SprayJsonSupport {
+trait EnigmaService extends HttpService with SprayJsonSupport with Alphabets {
+
+
+  def enigma: Enigma = {
+    Enigma(
+      (Rotor(realAlphabet, alphabetIII)
+        (Rotor(realAlphabet, alphabetII)
+          (Rotor(realAlphabet, alphabetI)(Reflector())
+            .rotateUpto('M'))
+          .rotateUpto('C'))
+        .rotateUpto('K')))
+  }
 
   val enigmaRoute =
     path("") {
@@ -15,7 +27,7 @@ trait EnigmaService extends HttpService with SprayJsonSupport {
         (decodeRequest(Gzip) | decodeRequest(NoEncoding)) {
           // unmarshal with in-scope unmarshaller
           entity(as[String]) { body =>
-            complete(body)
+            complete(enigma.transform( body))
           }
         }
 
